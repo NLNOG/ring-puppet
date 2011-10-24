55e29091ce41ce6365859fa18a0dd5f13af1f14f
NLNOG Ring-Admins <ring-admins@ring.nlnog.net>
2011-10-24 18:11:43 +0000

    added tdc machine

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -174,6 +174,11 @@ node 'atrato01' inherits ringnode {
     include smokeping::slave
 }
 
+node 'tdc01' inherits ringnode {
+    include smokeping::slave
+}
+
+
 node 'master01' inherits ringmaster {
     $sp_owner = "Job Snijders"
     $sp_owner_email = "job@snijders-it.nl"
