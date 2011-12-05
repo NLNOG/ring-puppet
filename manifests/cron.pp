8b9e8c3cdbcdd5f8d6557aeda372fc1f3980c2a9
Job Snijders <job@instituut.net>
2011-12-05 22:45:52 +0100

    fixed typo

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,6 +1,6 @@
 class mastercronjobs {
     cron { pull_from_repo:
-        command => "bash -c '( msg=`cd /etc/puppet/environments/production && /usr/bin/git pull  origin master  && /usr/bin/git reset  --hard && /usr/bin/git clean  -d -x -f && touch .puppet-sync-stamp 2>&1 > /tmp/logfail.log`; if [ ! -z "$msg" ] ; then echo "$msg" | /usr/bin/mail -s "`/bin/uname -n`:$LOGNAME:`/bin/date`: seems that repo copy on master01 failed" ring-admins@ring.nlnog.net; fi )'",
+        command => "bash -c '( msg=`cd /etc/puppet/environments/production && /usr/bin/git pull  origin master  && /usr/bin/git reset  --hard && /usr/bin/git clean  -d -x -f && touch .puppet-sync-stamp 2>&1 > /tmp/logfail.log`; if [ ! -z \"$msg\" ] ; then echo \"$msg\" | /usr/bin/mail -s \"`/bin/uname -n`:$LOGNAME:`/bin/date`: seems that repo copy on master01 failed\" ring-admins@ring.nlnog.net; fi )'",
         minute  => "*/5",
         user    => root,
         ensure  => present,
