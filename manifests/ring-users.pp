de357743e5925efa937fdf6bb3a8fe50b5cc1e29
Job Snijders <job@instituut.net>
2011-12-06 14:45:24 +0100

    test

--- manifests/ring-users.pp
+++ manifests/ring-users.pp
@@ -1,8 +1,22 @@
+# Class; ring_users
+#
+# This class creates all normal users on all ring nodes and adds their ssh_keys to the
+# authorized_keys file
+#
+# Parameters:
+#   none
+# Actions:
+#   Configures or removes all normal users on a ringnode
+# Requires:
+#   - add_user from classes/user.pp
+#   - authorized_keys from classes/user.pp
+
 class ring_users {
 
 # This is the file that manages all users and sshkeys
 # if you want to remove a user add 'ensure => absent' to the add_user block
 # if you want to remove or add a key, just add it to the appropiate array under the correct user
+
 
 ##### BEGIN intouch #####
 
