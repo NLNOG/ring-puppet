91f52a02b664a8b6a6f9f1a76e67f14436b7a5fe
Job Snijders <job@instituut.net>
2011-12-13 14:11:20 +0100

    added documentation

--- manifests/classes/ssh.pp
+++ manifests/classes/ssh.pp
@@ -1,9 +1,18 @@
+# Class ssh
+# this class distributes all ssh host keys to all hosts which include this
+# class
+#
 class ssh {
 
     file { "/etc/ssh/ssh_known_hosts":
         ensure => present,
         mode => 644,
     }
+
+    # collect a host key as a virtual exported resource
+    # we only need DSA and the name + alias are the first
+    # two names in /etc/ssh/ssh_known_hosts 
+    # which helps tab completition
 
     @@sshkey { 
         "${hostname}": 
@@ -19,7 +28,12 @@ class ssh {
 #            type => ssh-rsa,
 #            key => "${sshrsakey}";
 
+    # realize all the virtual exported resources
+    
     Sshkey <<| |>>
+
+    # remove all ssh host keys that are not managed by puppet
+    # this keeps stuff nice and clean
 
     resources { sshkey:
         purge => true,
