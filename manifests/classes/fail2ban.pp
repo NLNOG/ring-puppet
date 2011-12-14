adc624ece8efa432673daad8f3aeb603e99faedb
Job Snijders <job@instituut.net>
2011-12-14 16:27:44 +0100

    fixing fail2ban i hope

--- manifests/classes/fail2ban.pp
+++ manifests/classes/fail2ban.pp
@@ -4,17 +4,16 @@ class fail2ban-whitelist {
 
     package { "fail2ban": ensure => present }
 
-    exec { "echo -e \"[DEFAULT]\nignoreip = $(grep ^[0-9] /etc/hosts|cut -d' ' -f1|cut -f1|sort -n|grep -v :|tr '\n' ' ')\" > /etc/fail2ban/jail.local":
-        cwd => "/etc/fail2ban",
-        creates => "/etc/fail2ban/jail.local",
+    file { "/etc/fail2ban/jail.local":
+        require => Package["fail2ban"],
         subscribe => File["/etc/hosts"],
         alias => "create_jail_local",
-        require => Package["fail2ban"],
+        contents => template("fail2ban-jail.local.erb"),
     }
 
     service { fail2ban:
         ensure => running,
-        subscribe => Exec["create_jail_local"]
+        subscribe => File["create_jail_local"]
     }
 
 }
