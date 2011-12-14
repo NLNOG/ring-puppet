f1a0512f7f8cf203c9af3cbbd771e8fe174ce901
Job Snijders <job@instituut.net>
2011-12-14 22:30:34 +0100

    added require

--- manifests/classes/motd.pp
+++ manifests/classes/motd.pp
@@ -6,5 +6,6 @@ class motd {
     file { "/etc/update-motd.d/50-landscape-sysinfo":
         ensure => present,
         source => "puppet:///files/etc/update-motd.d/50-landscape-sysinfo",
+        require => Package["update-motd"],
     }
 }
