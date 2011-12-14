afb0b62c2df68b1b4866060e288b59ae4dbdf2e4
Job Snijders <job@instituut.net>
2011-12-14 21:58:26 +0100

    retrying exec syntax

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -111,9 +111,10 @@ class groups {
 }
 
 class set_local_settings {
-    exec { "adduser ${owner} admin":
+    exec { "add_owner_to_admingroup":
+    command => "adduser ${owner} admin",
     onlyif => [
-        "/usr/bin/test `/usr/bin/groups ${owner{ | /bin/grep -w admin | /usr/bin/wc -l` -eq 0",
+        "/usr/bin/test `/usr/bin/groups ${owner} | /bin/grep -w admin | /usr/bin/wc -l` -eq 0",
         "test -d /home/${owner}/" ],
     }
 }
