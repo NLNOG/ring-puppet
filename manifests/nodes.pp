b71dbe4150cc7a1750b48f7a9533f91d4107e5dc
Job Snijders <job@instituut.net>
2011-12-14 12:23:28 +0100

    obviously i typo

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -9,7 +9,7 @@ node basenode {
     include etcfiles
     include local_binaries
     include munin::client
-    munin::plugin { ["users"],
+    munin::plugin { ["users"]:
     }
     include nlnogrepokey
     include ssh
