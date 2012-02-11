a972d95c6ae720b1d337812c21076c2291a70cd6
Job Snijders <job@instituut.net>
2012-02-11 13:41:05 +0100

    converting unbound to upstart

--- manifests/classes/resolving.pp
+++ manifests/classes/resolving.pp
@@ -14,10 +14,23 @@ nameserver ::1
 
     package { "unbound":
         ensure => latest,
+        before  => File["/etc/init.d/unbound"],
+    }
+
+    file { "/etc/init/unbound.conf":
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        source  => "puppet:///files/etc/init/unbound.conf",
+    }
+
+    file { "/etc/init.d/unbound":
+        ensure  => absent,
     }
 
     service { "unbound":
-        ensure => running,
-        require => Package["unbound"],
+        ensure      => running,
+        require     => [ Package["unbound"], File["/etc/init/unbound.conf"] ],
+        provider    => upstart,
     }
 }
