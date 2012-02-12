f65a8f2cc7925e9a11f2052cfe69a435132320f4
Job Snijders <job@instituut.net>
2012-02-12 17:24:44 +0100

    another letter bites the dust

--- manifests/classes/nagios_services.pp
+++ manifests/classes/nagios_services.pp
@@ -30,7 +30,7 @@ class nagios_services {
         dependent_service_description   => "check_ssh6",
         host_name                       => "${fqdn}",
         dependent_host_name             => "${fqdn}",
-        notification_failure_criteria       => "w,u,c,r",
+        notification_failure_criteria       => "w,u,c",
     }
 }
 
