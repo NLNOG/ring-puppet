9ac13ad55122250b0ac32ac410edabec57d09cab
Job Snijders <job@instituut.net>
2012-02-12 23:10:57 +0100

    another test

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -115,7 +115,7 @@ node 'staging01' inherits ringnode {
     include nagios::target
     include nagios_services
     include set_local_settings
-    User <| group == "ring-admins" or group == "ring-users" |>
+    Add_user <| groups == "ring-admins" or groups == "ring-users" |>
 }
 
 node 'staging02' inherits ringnode {
