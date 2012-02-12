2ac8daa88870085101ad6b3fac31bde777365f0e
Job Snijders <job@instituut.net>
2012-02-12 17:00:39 +0100

    move nagios stuff to separate file and document a bit

---
+++ manifests/classes/nagios_services.pp
@@ -0,0 +1,34 @@
+# add things that should be checked here                                        
+class nagios_services {                                                 
+
+    # this will create a nagios_service for both IPv4 pings and IPv6 pings
+    # towards the FQDN of the node that's including this class
+    # names of the services: check_ping and check_ping6
+    nagios::service::ping { $name: }                      
+
+    # this will create a nagios_service for both IPv4 ssh check and IPv6 ssh check
+    # towards the FQDN of the node that's including this class
+    # names of the services: check_ssh_4 and check_ssh_6
+    nagios::service::ssh { $name: } 
+
+    # these virtual exported resources will create a dependeny between
+    # the IPv4 ping check and the IPv4 SSH check
+    # thus not alerting for ssh if ping fails first
+    @@nagios_servicedependency { "${fqdn}_dep_ssh_4": 
+        service_description             => "check_ping",
+        dependent_service_description   => "check_ssh_4",
+        host_name                       => "${fqdn}",
+        dependent_host_name             => "${fqdn}",
+    }
+
+    # these virtual exported resources will create a dependeny between
+    # the IPv6 ping check and the IPv6 SSH check
+    # thus not alerting for ssh if ping fails first
+    @@nagios_servicedependency { "${fqdn}_dep_ssh_6":
+        service_description             => "check_ping6",
+        dependent_service_description   => "check_ssh_6",
+        host_name                       => "${fqdn}",
+        dependent_host_name             => "${fqdn}",
+    }
+}
+
