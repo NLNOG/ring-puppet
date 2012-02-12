52923684df87ea4980148ed69b3632675a42def7
Job Snijders <job@instituut.net>
2012-02-12 17:04:57 +0100

    caught some wrong assumptions

--- manifests/classes/nagios_services.pp
+++ manifests/classes/nagios_services.pp
@@ -8,7 +8,7 @@ class nagios_services {
 
     # this will create a nagios_service for both IPv4 ssh check and IPv6 ssh check
     # towards the FQDN of the node that's including this class
-    # names of the services: check_ssh_4 and check_ssh_6
+    # names of the services: check_ssh4 and check_ssh6
     nagios::service::ssh { $name: } 
 
     # these virtual exported resources will create a dependeny between
@@ -16,7 +16,7 @@ class nagios_services {
     # thus not alerting for ssh if ping fails first
     @@nagios_servicedependency { "${fqdn}_dep_ssh_4": 
         service_description             => "check_ping",
-        dependent_service_description   => "check_ssh_4",
+        dependent_service_description   => "check_ssh4",
         host_name                       => "${fqdn}",
         dependent_host_name             => "${fqdn}",
     }
@@ -26,7 +26,7 @@ class nagios_services {
     # thus not alerting for ssh if ping fails first
     @@nagios_servicedependency { "${fqdn}_dep_ssh_6":
         service_description             => "check_ping6",
-        dependent_service_description   => "check_ssh_6",
+        dependent_service_description   => "check_ssh6",
         host_name                       => "${fqdn}",
         dependent_host_name             => "${fqdn}",
     }
