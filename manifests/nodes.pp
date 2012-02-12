c0dcbe8a6cec5d3255d130c4006a3ff0dd1bd448
Job Snijders <job@instituut.net>
2012-02-12 16:37:49 +0100

    test depending ssh on ping (and ssh6 on ping6)

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -99,6 +99,18 @@ class no-apache2 {
 class nagios_services {
     nagios::service::ping { $name: }
     nagios::service::ssh { $name: }
+    @@nagios_servicedependency { "${title}_${name}_ssh_6":
+        service_description             => "check_ping6",
+        dependent_service_description   => "check_ssh_6",
+        host_name                       => "${name}",
+        dependent_host_name             => "${name}",
+    }
+    @@nagios_servicedependency { "${title}_${name}_ssh_4":
+        service_description             => "check_ping",
+        dependent_service_description   => "check_ssh_4",
+        host_name                       => "${name}",
+        dependent_host_name             => "${name}",
+    }
 }
 
 ## define all groups
