1d70f7dd0dabd7310c9e039fd3ad113958a868a8
Job Snijders <job@instituut.net>
2012-03-02 15:44:16 +0100

    do it differently, purely in /etc/smokeping/config

--- modules/smokeping/manifests/master.pp
+++ modules/smokeping/manifests/master.pp
@@ -33,18 +33,16 @@ class smokeping::master {
         "/etc/smokeping/config.d/Targets_header_v6":
             content => template("smokeping/targetsheader_v6.erb"),
             mode => 0644, owner => root, group => root,
-            ensure  => absent,
         }
  
     concatenated_file { "/etc/smokeping/config.d/Targets_v4":
         dir => "$SP_NODESDIR/v4",
     }
 
-# uncomment to enable ipv6 support agian
-#    concatenated_file { "/etc/smokeping/config.d/Targets_v6":
-#        dir => "$SP_NODESDIR/v6",
-#       require => File["/etc/smokeping/config.d/Targets_header_v6"],
-#    }
+    concatenated_file { "/etc/smokeping/config.d/Targets_v6":
+        dir => "$SP_NODESDIR/v6",
+       require => File["/etc/smokeping/config.d/Targets_header_v6"],
+    }
 
     file {
         "/etc/apache2/conf.d/smokeping":
