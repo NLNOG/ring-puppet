9152849100c51e9df3d01c515715503de0a00af3
Job Snijders <job@instituut.net>
2012-03-06 13:49:19 +0100

    move check_ping to exception for ubuntu

--- modules/nagios/manifests/defaults/commands.pp
+++ modules/nagios/manifests/defaults/commands.pp
@@ -29,7 +29,9 @@ class nagios::defaults::commands {
             command_line => '$USER1$/check_tcp -H $ARG1$ -p $ARG2$';
           check_jabber:
             command_line => '$USER1$/check_jabber -H $ARG1$';
-          check_ping6:
+           check_ping:
+            command_line => '$USER1$/check_ping -4 -H $HOSTADDRESS$ -w $ARG1$ -c $ARG2$';
+         check_ping6:
             command_line => '$USER1$/check_ping -6 -H $HOSTNAME$ -w $ARG1$ -c $ARG2$';
           check_ssh_6:
             command_line => '$USER1$/check_ssh -6 $HOSTNAME$';
@@ -39,9 +41,7 @@ class nagios::defaults::commands {
         nagios_command {
           check_dummy:
             command_line => '$USER1$/check_dummy $ARG1$';
-         check_ping:
-            command_line => '$USER1$/check_ping -4 -H $HOSTADDRESS$ -w $ARG1$ -c $ARG2$';
-          check-host-alive:
+         check-host-alive:
             command_line => '$USER1$/check_ping -H $HOSTADDRESS$ -w 5000,100% -c 5000,100% -p 1';
           check_tcp:
             command_line => '$USER1$/check_tcp -H $HOSTADDRESS$ -p $ARG1$';
