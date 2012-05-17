b53c259d03a7a82e83cf8f7ca73d87227ba46899
Job Snijders <job@instituut.net>
2012-05-17 12:21:32 +0200

    try and fix syslogng

--- modules/syslog_ng/manifests/client.pp
+++ modules/syslog_ng/manifests/client.pp
@@ -10,14 +10,12 @@ class syslog_ng::client inherits syslog_ng {
     file { 
         "/etc/syslog-ng/syslog-ng.conf":
             notify  => Service["syslog-ng"],
-            content => $lsbdistcodename ? {
-                maverick    => template("syslog_ng/syslog-ng.conf.client.erb.maverick"),
-                precise     => template("syslog_ng/syslog-ng.conf.client.erb.precise"),
-            }
+            content => template("syslog_ng/syslog-ng.conf.client.erb.${lsbdistcodename}");
+
         "/etc/logrotate.d/syslog-ng":
-        owner   => root,
-        group   => root,
-        mode    => 0755,
+            owner   => root,
+            group   => root,
+            mode    => 0755,
             source => "puppet:///modules/syslog_ng/syslog-logrotate";
     } # file
 
