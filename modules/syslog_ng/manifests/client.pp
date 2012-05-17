14857f3220f3107bd1b1e86c900db85e597a5224
Martin Pels <martin@rodecker.nl>
2012-05-17 11:58:53 +0200

    new syslog client config for precise

--- modules/syslog_ng/manifests/client.pp
+++ modules/syslog_ng/manifests/client.pp
@@ -10,7 +10,10 @@ class syslog_ng::client inherits syslog_ng {
     file { 
         "/etc/syslog-ng/syslog-ng.conf":
             notify  => Service["syslog-ng"],
-            content => template("syslog_ng/syslog-ng.conf.client.erb");
+            content => $lsbdistcodename ? {
+                maverick    => template("syslog_ng/syslog-ng.conf.client.erb.maverick"),
+                precise     => template("syslog_ng/syslog-ng.conf.client.erb.precise"),
+            }
         "/etc/logrotate.d/syslog-ng":
         owner   => root,
         group   => root,
