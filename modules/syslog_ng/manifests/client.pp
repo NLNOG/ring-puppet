50a33a0d5b91a6001379566d930cabe6885296c7
Martin Pels <martin@rodecker.nl>
2012-05-17 12:16:30 +0200

    syntax error fix

--- modules/syslog_ng/manifests/client.pp
+++ modules/syslog_ng/manifests/client.pp
@@ -14,12 +14,15 @@ class syslog_ng::client inherits syslog_ng {
                 maverick    => template("syslog_ng/syslog-ng.conf.client.erb.maverick"),
                 precise     => template("syslog_ng/syslog-ng.conf.client.erb.precise"),
             }
+    }
+
+    file {
         "/etc/logrotate.d/syslog-ng":
         owner   => root,
         group   => root,
         mode    => 0755,
             source => "puppet:///modules/syslog_ng/syslog-logrotate";
-    } # file
+    }
 
     file {
         "/etc/init.d/syslog-ng":
