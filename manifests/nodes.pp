f04571f2b53e52c43ac63c7bd50a533acab7c108
Job Snijders <job@instituut.net>
2012-06-20 21:51:29 +0200

    define software for container nodes

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -63,6 +63,7 @@ node 'container01' inherits basenode {
     include syslog_ng::client
     include nagios_services
     include nagios::target::fqdn
+    include container_software
     munin::plugin { ["apache_accesses", "apache_processes", "apache_volume"]:
     }
 }
