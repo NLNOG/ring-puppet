12d154ff8e8e963f6f065f5ddc9e78dac31e16c2
Job Snijders <job@instituut.net>
2012-11-07 18:45:14 +0100

    test

--- manifests/ring-users.pp
+++ manifests/ring-users.pp
@@ -16,6 +16,15 @@
 
 class users::virtual::ring_users {
 
+##### BEGIN former users #####
+
+@add_user { 'ic-hosting':
+    email   => 'info@ic-hosting.nl',
+    company => 'IC Hosting',
+    uid => 5014,
+    groups  => ['ring-users'],
+    present => absent,
+}
 
 ##### BEGIN intouch #####
 
