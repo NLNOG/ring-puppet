c627e95108e8e08c38d03c3a1ec2d972c35a0d64
Job Snijders <job@instituut.net>
2012-11-11 14:55:12 +0100

    testing

--- modules/mysql/manifests/config/performance.pp
+++ modules/mysql/manifests/config/performance.pp
@@ -1,25 +1,25 @@
 class mysql::config::performance {
   # force use of system defaults
   mysql::config {
-#    'key_buffer':                      ensure => absent;
-    'max_allowed_packet':              ensure => absent;
-    'table_cache':                     ensure => absent;
-    'sort_buffer_size':                ensure => absent;
-    'read_buffer_size':                ensure => absent;
-    'read_rnd_buffer_size':            ensure => absent;
-    'net_buffer_length':               ensure => absent;
-    'myisam_sort_buffer_size':         ensure => absent;
-    'thread_cache_size':               ensure => absent;
-    'query_cache_size':                ensure => absent;
-    'thread_concurrency':              ensure => absent;
-    'thread_stack':                    ensure => absent;
-    'isamchk/key_buffer':              ensure => absent;
-    'isamchk/sort_buffer_size':        ensure => absent;
-    'isamchk/read_buffer':             ensure => absent;
-    'isamchk/write_buffer':            ensure => absent;
-    'myisamchk/key_buffer':            ensure => absent;
-    'myisamchk/sort_buffer_size':      ensure => absent;
-    'myisamchk/read_buffer':           ensure => absent;
-    'myisamchk/write_buffer':          ensure => absent;
+# #    'key_buffer':                      ensure => absent;
+#     'max_allowed_packet':              ensure => absent;
+#     'table_cache':                     ensure => absent;
+#     'sort_buffer_size':                ensure => absent;
+#     'read_buffer_size':                ensure => absent;
+#     'read_rnd_buffer_size':            ensure => absent;
+#     'net_buffer_length':               ensure => absent;
+#     'myisam_sort_buffer_size':         ensure => absent;
+#     'thread_cache_size':               ensure => absent;
+#     'query_cache_size':                ensure => absent;
+#     'thread_concurrency':              ensure => absent;
+#     'thread_stack':                    ensure => absent;
+#     'isamchk/key_buffer':              ensure => absent;
+#     'isamchk/sort_buffer_size':        ensure => absent;
+#     'isamchk/read_buffer':             ensure => absent;
+#     'isamchk/write_buffer':            ensure => absent;
+#     'myisamchk/key_buffer':            ensure => absent;
+#     'myisamchk/sort_buffer_size':      ensure => absent;
+#     'myisamchk/read_buffer':           ensure => absent;
+#     'myisamchk/write_buffer':          ensure => absent;
   }
 }
