9c34f7e9aa0b4844be9f9d3516b2103c08697c11
Martin Pels <martin@rodecker.nl>
2012-11-16 22:26:58 +0100

    nagios latency fix rootlu

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1122,6 +1122,7 @@ node 'rootlu01' inherits ringnode {
     include nagios_services
     include set_local_settings
 	include users
+    $nagios_ping_rate = '!200.0,20%!400.0,60%'
 }
 
 node 'globalaxs01' inherits ringnode {
