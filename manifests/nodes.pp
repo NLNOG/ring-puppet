668537f1ba01730e9fee0f5348f6e8ac88ec6681
Martin Pels <martin@rodecker.nl>
2012-11-25 13:43:35 +0100

    rootlu nagios adjustment

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1122,7 +1122,7 @@ node 'rootlu01' inherits ringnode {
     include nagios_services
     include set_local_settings
 	include users
-    $nagios_ping_rate = '!200.0,20%!400.0,60%'
+    $nagios_ping_rate = '!300.0,20%!400.0,60%'
 }
 
 node 'globalaxs01' inherits ringnode {
