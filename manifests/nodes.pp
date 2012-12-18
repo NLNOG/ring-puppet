d0734ce1e7db2f4ac5ff09b176496508f58e0ab1
Job Snijders <job@instituut.net>
2012-12-18 15:46:10 +0200

    move staging01 to container04

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -346,6 +346,20 @@ node 'container04.infra' inherits infranode {
         container   => "${hostname}",
         ensure      => present,
     }
+    kvm::virtual_machine { 'staging01':
+        fqdn        => 'bit01.ring.nlnog.net',
+        ip          => '213.154.249.164', # ipv6 address is 2001:7b8:d05::164
+        netmask     => '255.255.255.240',
+        dns         => '8.8.8.8',
+        gateway     => '213.154.249.161',
+        memory      => '512',
+        disksize    => '20',
+        rootsize    => '19968',
+        bridge      => 'virbr1',
+        container   => "${hostname}",
+        ensure      => present,
+    }
+
 
 }
 
