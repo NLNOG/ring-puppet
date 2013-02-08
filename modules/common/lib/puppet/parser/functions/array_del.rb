5478610463200477591cb9b515974c407df9753b
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2013-02-08 23:01:29 +0100

    module common, second parameter of array_del can be a string or an array

--- modules/common/lib/puppet/parser/functions/array_del.rb
+++ modules/common/lib/puppet/parser/functions/array_del.rb
@@ -3,9 +3,11 @@ Puppet::Parser::Functions::newfunction(
   :type => :rvalue,
   :doc => "Deletes items from an array
 
-    Example: array_del(['a','b'],'b') -> ['a']"
+    Example: array_del(['a','b'],'b') -> ['a']
+             array_del(['a','b', 'c'],['b', 'c']) -> ['a']"
 ) do |args|
   raise Puppet::ParseError, 'array_del() needs two arguments' if args.length != 2
-  (res=args[0].dup).to_a.delete(args[1])
+  res = args[0].dup.to_a
+  args[1].each { |x| res.delete(x) }
   res
 end
