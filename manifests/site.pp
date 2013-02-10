d56f6c698a64f69227e84e2fc4f23d428b668f7a
Martin Pels <martin@rodecker.nl>
2013-02-10 14:22:31 +0100

    ssl for mysql

--- manifests/site.pp
+++ manifests/site.pp
@@ -18,6 +18,10 @@ filebucket { main: server => puppet }
 File { backup => main }
 Exec { path => "/usr/bin:/usr/sbin/:/bin:/sbin" }
 
+$cacert = "/var/lib/puppet/ssl/certs/ca.pem"
+$signedcert = "/var/lib/puppet/ssl/certs/${fqdn}.pem"
+$certkey = "/var/lib/puppet/ssl/private_keys/${fqdn}.pem"
+
 Package {
     provider => $operatingsystem ? {
         debian => apt,
--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -21,4 +21,37 @@ class mysql (
     name   => $package_name,
   }
 
+  $configdir = "/etc/mysql/conf.d"
+  $ssldir = "/etc/mysql/ssl"
+  $ssldirca = "$ssldir/ca.pem"
+  $ssldircert = "$ssldir/${fqdn}.pem"
+  $ssldirkey = "$ssldir/key-${fqdn}.pem"
+
+  file { 
+    "/etc/mysql":
+        ensure => directory,
+        owner => mysql, group => mysql, mode => 644;
+    $configdir:
+        ensure => directory,
+        owner => mysql, group => mysql, mode => 644,
+        require => Package["mysql-server"];
+    $ssldir:
+        ensure => directory,
+        owner => mysql, group => mysql, mode => 600;
+    $ssldirca:
+        ensure => present,
+        source => $cacert,
+        owner => mysql, group => mysql, mode => 600;
+    $ssldircert:
+        ensure => present,
+        source => $signedcert,
+        owner => mysql, group => mysql, mode => 600;
+    $ssldirkey:
+        ensure => present,
+        source => $certkey,
+        owner => mysql, group => mysql, mode => 600;
+    "/etc/mysql/conf.d/ssl.conf":
+        content => template("mysql/ssl.conf.erb")
+        owner => mysql, group => mysql, mode => 644;
+  }
 }
