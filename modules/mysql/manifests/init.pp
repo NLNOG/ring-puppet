69bed094c6534af14a1993c3609842179987527d
Job Snijders <job@instituut.net>
2013-03-14 22:04:28 +0100

    new mysql module

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,57 +1,11 @@
-# Class: mysql
+# mysql module
 #
-#   This class installs mysql client software.
-#
-# Parameters:
-#   [*client_package_name*]  - The name of the mysql client package.
-#
-# Actions:
-#
-# Requires:
-#
-# Sample Usage:
-#
-class mysql (
-  $package_name   = $mysql::params::client_package_name,
-  $package_ensure = 'present'
-) inherits mysql::params {
+# Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
+# Copyright 2008, admin(at)immerda.ch
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+# Copyright 2009-2010, Riseup Labs <http://riseuplabs.org>
+# Pietro Ferrari <pietro@riseup.net>
+# Micah Anderson <micah@riseup.net>
 
-  package { 'mysql_client':
-    ensure => $package_ensure,
-    name   => $package_name,
-  }
-
-  $configdir = "/etc/mysql/conf.d"
-  $ssldir = "/etc/mysql/ssl"
-  $ssldirca = "$ssldir/ca.pem"
-  $ssldircert = "$ssldir/${fqdn}.pem"
-  $ssldirkey = "$ssldir/key-${fqdn}.pem"
-
-  file { 
-    "/etc/mysql":
-        ensure => directory,
-        owner => mysql, group => mysql, mode => 644;
-    $configdir:
-        ensure => directory,
-        owner => mysql, group => mysql, mode => 644,
-        require => Package["mysql-server"];
-    $ssldir:
-        ensure => directory,
-        owner => mysql, group => mysql, mode => 600;
-    $ssldirca:
-        ensure => present,
-        source => $cacert,
-        owner => mysql, group => mysql, mode => 600;
-    $ssldircert:
-        ensure => present,
-        source => $signedcert,
-        owner => mysql, group => mysql, mode => 600;
-    $ssldirkey:
-        ensure => present,
-        source => $certkey,
-        owner => mysql, group => mysql, mode => 600;
-    "/etc/mysql/conf.d/ssl.conf":
-        content => template("mysql/ssl.conf.erb"),
-        owner => mysql, group => mysql, mode => 644;
-  }
-}
--- modules/mysql/manifests/server.pp
+++ modules/mysql/manifests/server.pp
@@ -1,53 +1,33 @@
-# Class: mysql::server
-#
-# manages the installation of the mysql server.  manages the package, service,
-# my.cnf
-#
-# Parameters:
-#   [*package_name*] - name of package
-#   [*service_name*] - name of service
-#   [*config_hash*]  - hash of config parameters that need to be set.
-#
-# Actions:
-#
-# Requires:
-#
-# Sample Usage:
-#
+# manage a mysql server
 class mysql::server (
-  $package_name     = $mysql::params::server_package_name,
-  $package_ensure   = 'present',
-  $service_name     = $mysql::params::service_name,
-  $service_provider = $mysql::params::service_provider,
-  $config_hash      = {},
-  $enabled          = true,
-  $manage_service   = true
-) inherits mysql::params {
-
-  Class['mysql::server'] -> Class['mysql::config']
-
-  $config_class = { 'mysql::config' => $config_hash }
-
-  create_resources( 'class', $config_class )
-
-  package { 'mysql-server':
-    ensure => $package_ensure,
-    name   => $package_name,
+  $manage_shorewall  = false,
+  $manage_munin      = false,
+  $manage_nagios     = false,
+  $backup_cron       = false,
+  $optimize_cron     = false,
+  $backup_dir        = '/var/backups/mysql',
+  $manage_backup_dir = true,
+  $nagios_notcp      = false
+) {
+  case $::operatingsystem {
+    gentoo:  { include mysql::server::gentoo }
+    centos:  { include mysql::server::centos }
+    debian:  { include mysql::server::debian }
+    default: { include mysql::server::base }
   }
 
-  if $enabled {
-    $service_ensure = 'running'
-  } else {
-    $service_ensure = 'stopped'
-  }
-
-  if $manage_service {
-    service { 'mysqld':
-      ensure   => $service_ensure,
-      name     => $service_name,
-      enable   => $enabled,
-      require  => Package['mysql-server'],
-      provider => $service_provider,
+  if $manage_munin and $::mysql_exists == 'true' {
+    case $::operatingsystem {
+      debian:  { include mysql::server::munin::debian }
+      default: { include mysql::server::munin::default }
     }
+  }
+
+  if $manage_nagios and $::mysql_exists == 'true' {
+    include mysql::server::nagios
+  }
+
+  if $manage_shorewall {
+    include shorewall::rules::mysql
   }
 }
