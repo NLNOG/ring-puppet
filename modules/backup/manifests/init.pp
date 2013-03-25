b62d0dc00be6f72e181a2ffab08db71311287472
Job Snijders <job@instituut.net>
2013-03-25 16:43:05 +0100

    update bit

--- modules/backup/manifests/init.pp
+++ modules/backup/manifests/init.pp
@@ -31,4 +31,11 @@ class backup::client {
         minute => fqdn_rand(50),
         tag => 'rdiff',
     }
+    @@cron { "backupclean_${fqdn}":
+        command => "rdiff-backup --remove-older-than 12W /backups/${fqdn}/",
+        user => root,
+        hour => [ fqdn_rand(12), fqdn_rand(12) + 12 ],
+        minute => fqdn_rand(50),
+        tag => 'rdiff',
+    }
 }
