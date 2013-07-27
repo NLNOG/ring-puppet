3200ed0c8d83c08a155bbcd42e7787b2e008baeb
Job Snijders <job@instituut.net>
2013-07-27 14:57:16 +0200

    remove upstart for unbound

--- manifests/classes/resolving.pp
+++ manifests/classes/resolving.pp
@@ -18,21 +18,22 @@ nameserver ::1
     }
 
     file { "/etc/init/unbound.conf":
-        owner   => root,
-        group   => root,
-        mode    => 644,
-        source  => "puppet:///files/etc/init/unbound.conf",
-    }
-
-    file { "/etc/init.d/unbound":
         ensure  => absent,
     }
 
+    file { "/etc/init.d/unbound":
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        source  => "puppet:///files/etc/init.d/unbound",
+    }
+
+
     service { "unbound":
         ensure      => running,
-        require     => [ Package["unbound"], File["/etc/init/unbound.conf"] ],
-        provider    => upstart,
+        require     => [ Package["unbound"], File["/etc/init.d/unbound"] ],
         subscribe   => File["/etc/unbound/unbound.conf"],
+        hasrestart  => true,
     }
 
     file { "/etc/unbound/unbound.conf":
