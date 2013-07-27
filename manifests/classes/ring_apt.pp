db973ebc94beb336e539e968346c3ada397d7c6b
Job Snijders <job@instituut.net>
2013-07-27 16:06:00 +0200

    forgot a comma

--- manifests/classes/ring_apt.pp
+++ manifests/classes/ring_apt.pp
@@ -2,14 +2,14 @@ class ring::apt {
     include apt
     
     apt::source { "ubuntu_archiv_precise":
-        location        => "mirror://mirrors.ubuntu.com/mirrors.txt"
+        location        => "mirror://mirrors.ubuntu.com/mirrors.txt",
         release         => "precise",
         repos           => "main restricted universe multiverse",
         include_src     => false
     }
 
     apt::source { "ubuntu_archiv_precise-updates":
-        location        => "mirror://mirrors.ubuntu.com/mirrors.txt"
+        location        => "mirror://mirrors.ubuntu.com/mirrors.txt",
         release         => "precise-updates",
         repos           => "main restricted universe multiverse",
         include_src     => false
