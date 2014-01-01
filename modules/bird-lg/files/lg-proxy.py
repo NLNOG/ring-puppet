ea03d1fec2f2406548e632e96969959e5e853a9a
Job Snijders <job@instituut.net>
2014-01-01 14:30:31 +0100

    clean up python code

--- modules/bird-lg/files/lg-proxy.py
+++ modules/bird-lg/files/lg-proxy.py
@@ -30,34 +30,34 @@ from flask import Flask, request, abort
 app = Flask(__name__)
 app.config.from_pyfile('lg-proxy.cfg')
 
+
 def check_accesslist():
-    if  app.config["ACCESS_LIST"] and request.remote_addr not in app.config["ACCESS_LIST"]:
+    if app.config["ACCESS_LIST"] and \
+            request.remote_addr not in app.config["ACCESS_LIST"]:
         abort(401)
+
 
 @app.route("/traceroute")
 @app.route("/traceroute6")
 def traceroute():
     check_accesslist()
-
     src = []
     if request.path == '/traceroute6':
-	o = "-6"
-	if app.config.get("IPV6_SOURCE",""):
-	     src = [ "-s",  app.config.get("IPV6_SOURCE") ]
-
+        o = "-6"
+    if app.config.get("IPV6_SOURCE", ""):
+        src = ["-s",  app.config.get("IPV6_SOURCE")]
     else:
-	o = "-4"
-	if app.config.get("IPV4_SOURCE",""):
-	     src = [ "-s",  app.config.get("IPV4_SOURCE") ]
+        o = "-4"
+    if app.config.get("IPV4_SOURCE", ""):
+        src = ["-s",  app.config.get("IPV4_SOURCE")]
 
-    query = request.args.get("q","")
+    query = request.args.get("q", "")
     query = unquote(query)
 
-    command = [ 'traceroute' , o ] + src + [ '-A', '-q1', '-N32', '-w1', '-m15', query ]
-    result = subprocess.Popen( command , stdout=subprocess.PIPE).communicate()[0].decode('utf-8', 'ignore').replace("\n","<br>")
-
+    command = ['traceroute', o] + src \
+        + ['-A', '-q1', '-N32', '-w1', '-m15', query]
+    result = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0].decode('utf-8', 'ignore').replace("\n", "<br>")
     return result
-
 
 
 @app.route("/bird")
@@ -65,11 +65,14 @@ def traceroute():
 def bird():
     check_accesslist()
 
-    if request.path == "/bird": b = BirdSocket(file="/run/bird/bird.ctl")
-    elif request.path == "/bird6": b = BirdSocket(file="/run/bird/bird6.ctl")
-    else: return "No bird socket selected"
+    if request.path == "/bird":
+        b = BirdSocket(file="/run/bird/bird.ctl")
+    elif request.path == "/bird6":
+        b = BirdSocket(file="/run/bird/bird6.ctl")
+    else:
+        return "No bird socket selected"
 
-    query = request.args.get("q","")
+    query = request.args.get("q", "")
     query = unquote(query)
 
     status, result = b.cmd(query)
@@ -81,4 +84,3 @@ def bird():
 if __name__ == "__main__":
     app.debug = True
     app.run("0.0.0.0")
-
