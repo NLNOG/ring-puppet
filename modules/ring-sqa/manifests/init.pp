514aaf07d4167e0d3bd53a5f393cb8bc1ca7a6d1
Job Snijders <jsnijders@ebay.com>
2014-07-22 14:29:26 +0200

    ring-sqa

---
+++ modules/ring-sqa/manifests/init.pp
@@ -0,0 +1,17 @@
+class ring_sqa {
+    package { "ring-sqa":
+        ensure      => latest,
+        provider    => "gem",
+    }
+
+    alternatives { "ruby":
+        ensure  => '/usr/bin/ruby1.9.1',
+        require => Package['ruby1.9.3'],
+    }
+
+    alternatives { "gem":
+        ensure  => '/usr/bin/gem1.9.1',
+        require => Package['ruby1.9.3'],
+    }
+
+}
