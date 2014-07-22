ff6f602bab6d8d461255a270f019a4fe0dff2994
Job Snijders <jsnijders@ebay.com>
2014-07-22 14:36:25 +0200

    fix

--- modules/ring_sqa/manifests/init.pp
+++ modules/ring_sqa/manifests/init.pp
@@ -1,15 +1,16 @@
 class ring_sqa {
-    package { "ring-sqa":
-        ensure      => latest,
-        provider    => "gem",
+
+    package { 'ring-sqa':
+        ensure      => 'latest',
+        provider    => 'gem',
     }
 
-    alternatives { "ruby":
+    alternatives { 'ruby':
         ensure  => '/usr/bin/ruby1.9.1',
         require => Package['ruby1.9.3'],
     }
 
-    alternatives { "gem":
+    alternatives { 'gem':
         ensure  => '/usr/bin/gem1.9.1',
         require => Package['ruby1.9.3'],
     }
