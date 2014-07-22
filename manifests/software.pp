9119255f6c05763d98cfd40d0b04d2fc9eeb416c
Job Snijders <jsnijders@ebay.com>
2014-07-22 16:15:56 +0200

    push sqlite to all nodes

--- manifests/software.pp
+++ manifests/software.pp
@@ -224,6 +224,7 @@ class nettools {
     package { "sshfp": ensure => latest }
     package { "ringfpingd": ensure => latest }
     package { "ruby1.9.3": ensure => latest }
+    package { "libsqlite3-dev": ensure => latest }
 
     # packages we don't like
     package { "pppoe": ensure => purged }
