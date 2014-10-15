b37d251b3945c700e8041799da77edc40ca4d5b6
Martin Pels <martin@rodecker.nl>
2014-10-15 21:36:48 +0200

    removed rbnetwork02

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2239,16 +2239,6 @@ node 'nexellent01' inherits ringnode {
     include users
 }
 
-node 'rbnetwork02' inherits ringnode {
-    $owner = "rbnetwork"
-    $location = "47.387639,8.519944"
-    include amp_client                                                    
-    include nagios::target::fqdn                                                
-    include nagios_services                                                     
-    include set_local_settings                                                  
-    include users
-}
-
 node 'afilias01' inherits ringnode {
     $owner = "afilias"
     $location = "52.34398,4.82871"
