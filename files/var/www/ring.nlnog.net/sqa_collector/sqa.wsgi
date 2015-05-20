8f69eed8d8da28f60a7d68d5ba766095f0a40997
Job Snijders <job@instituut.net>
2015-05-20 17:56:26 +0100

    Add SQA collector flask placeholder

---
+++ files/var/www/ring.nlnog.net/sqa_collector/sqa.wsgi
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0, '/var/www/ring.nlnog.net/sqa_collector')
+
+from sqa_collector import app as application
---
+++ files/var/www/ring.nlnog.net/sqa_collector/sqa_collector.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import time
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def root():
+    print request.get_data()
+    blob = open('/tmp/sqacollector/%s-%s'
+        % (request.remote_addr, time.time()), 'w')
+    blob.write(request.get_data())
+    blob.close()
+    return "OK"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
