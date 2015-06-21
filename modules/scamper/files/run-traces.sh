5f5f112d2cb7ba4580dcd1783eea94b30ca67ecd
Job Snijders <job@instituut.net>
2015-06-21 18:42:37 +0200

    Set ownership of warts file

--- modules/scamper/files/run-traces.sh
+++ modules/scamper/files/run-traces.sh
@@ -5,3 +5,5 @@ filename="$(hostname)-$(date +%s).warts"
 /usr/bin/sc_attach -p 23456 -c 'trace' -i /etc/ring/node-list.txt -o /tmp/$$.${filename} && \
     mv /tmp/$$.${filename} /home/scamper/collected/${filename} && \
     gzip /home/scamper/collected/${filename}
+
+chown scamper.ring-users /home/scamper/collected/${filename}.gz
