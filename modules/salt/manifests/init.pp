bc5a6786bf3d570d180c20a95e0e4bc98fd043bd
Job Snijders <job@instituut.net>
2015-12-08 10:30:20 +0100

    Salt service ordering

--- modules/salt/manifests/init.pp
+++ modules/salt/manifests/init.pp
@@ -9,5 +9,13 @@ class salt {
 
     file {"/etc/salt/minion":
         content => "master_finger: '54:8b:81:be:b3:b8:39:df:e0:de:bf:55:03:b1:9b:f3'",
+        after => Package["salt"],
+        notify => Service["salt-minion"],
+    }
+
+    service { 'salt-minion':
+        ensure  => running,
+        enable  => true,
+        subscrube => File["/etc/salt/minion"],
     }
 }
