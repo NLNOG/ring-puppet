49506c7035ab0b77dab0b07f27b5dc1039275ac5
Job Snijders <job@instituut.net>
2015-12-08 10:32:55 +0100

    Swap after with before

--- modules/salt/manifests/init.pp
+++ modules/salt/manifests/init.pp
@@ -1,6 +1,7 @@
 class salt {
     package {["salt-minion"]:
         ensure  => installed,
+        before => Package["/etc/salt/minion"],
     }
     
     apt::ppa { 'ppa:saltstack/salt':
@@ -9,7 +10,6 @@ class salt {
 
     file {"/etc/salt/minion":
         content => "master_finger: '54:8b:81:be:b3:b8:39:df:e0:de:bf:55:03:b1:9b:f3'",
-        after => Package["salt-minion"],
         notify => Service["salt-minion"],
     }
 
