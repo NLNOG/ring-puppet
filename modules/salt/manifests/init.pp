8db6cde1b3779cc90f68ea1164edbc2032438dfc
Job Snijders <job@instituut.net>
2015-12-08 10:31:41 +0100

    Fix Salt requirements

--- modules/salt/manifests/init.pp
+++ modules/salt/manifests/init.pp
@@ -1,15 +1,15 @@
 class salt {
-    package {"salt":
+    package {["salt-minion"]:
         ensure  => installed,
     }
     
     apt::ppa { 'ppa:saltstack/salt':
-        before => Package["salt"],
+        before => Package["salt-minion"],
     }
 
     file {"/etc/salt/minion":
         content => "master_finger: '54:8b:81:be:b3:b8:39:df:e0:de:bf:55:03:b1:9b:f3'",
-        after => Package["salt"],
+        after => Package["salt-minion"],
         notify => Service["salt-minion"],
     }
 
@@ -17,5 +17,6 @@ class salt {
         ensure  => running,
         enable  => true,
         subscrube => File["/etc/salt/minion"],
+        require => Package["salt-minion"],
     }
 }
