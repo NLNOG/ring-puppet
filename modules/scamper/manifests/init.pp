f8e957d06c4b8e8e0f0258014ca3f26e5e14ce77
Job Snijders <job@instituut.net>
2016-01-03 00:49:41 +0100

    Fully describe cronjob which should be removed

--- modules/scamper/manifests/init.pp
+++ modules/scamper/manifests/init.pp
@@ -1,13 +1,15 @@
 class scamper {
 
     cron { "collect_all_traces":
+        user => "scamper",
+        command => "/home/scamper/run-traces.sh",
         ensure => absent,
-        user => scamper,
     }
 
     cron { "clean_scamper":
+        user => "scamper",
+        command => "find /home/scamper/collected/* -mtime +8 -exec rm {} \;",
         ensure => absent,
-        user => root,
     }
 
 }
