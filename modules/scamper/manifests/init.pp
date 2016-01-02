04bbccf1e0e5bf784e0bfc4f27d65229f2701abc
Job Snijders <job@instituut.net>
2016-01-03 00:47:04 +0100

    Retain user for absent crontabs

--- modules/scamper/manifests/init.pp
+++ modules/scamper/manifests/init.pp
@@ -2,10 +2,12 @@ class scamper {
 
     cron { "collect_all_traces":
         ensure => absent,
+        user => scamper,
     }
 
     cron { "clean_scamper":
         ensure => absent,
+        user => root,
     }
 
 }
