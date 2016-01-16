023c5c488589f35cd95a9fec74e1f9ef898f55a7
Martin Pels <martin@rodecker.nl>
2016-01-16 13:36:02 +0100

    reactivate ansible cronjob

--- modules/ansible/manifests/init.pp
+++ modules/ansible/manifests/init.pp
@@ -28,7 +28,7 @@ class ansible::client {
         command => "ansible-pull -d /etc/ansible/ring -U https://github.com/NLNOG/ring-ansible.git -i nodes -l $fqdn -c local playbook.yml >/dev/null 2>/dev/null",
         minute  => "$minute",
         user    => root,
-        ensure  => absent,
+        ensure  => present,
     }
 
     #exec { 'ansible_pull':
