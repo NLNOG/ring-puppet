f9043adbc3f196dc67cc4fd077a666f698eca07b
Martin Pels <martin@rodecker.nl>
2016-10-11 11:12:23 +0200

    add apt-get update

--- modules/ansible/manifests/init.pp
+++ modules/ansible/manifests/init.pp
@@ -33,6 +33,11 @@ class ansible::client {
         require  => Exec['apt-get update'],
     }
 
+    exec { apt-get update:
+        command => "/usr/bin/apt-get update",
+        onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
+    }
+
     apt::pin { ansible:
         packages    => 'ansible',
         priority    => '1001',
