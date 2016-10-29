61c1a49cb8f42969848542a41f08656bf2a78873
Martin Pels <martin@rodecker.nl>
2016-10-29 16:40:34 +0200

    netrouting

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -3776,3 +3776,9 @@ node 'franceix01' inherits ringnode {
     include set_local_settings
     include users
 }
+node 'netrouting01' inherits ringnode {
+    $owner = "netrouting"
+    $location = "51.8556047,4.301611800000046"
+    include set_local_settings
+    include users
+}
