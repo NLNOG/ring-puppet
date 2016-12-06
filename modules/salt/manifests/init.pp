8773b0917e60484cafc1f8dcd4c13d28f2b9627d
Martin Pels <martin@rodecker.nl>
2016-12-06 14:56:54 +0100

    remove salt-minion

--- modules/salt/manifests/init.pp
+++ modules/salt/manifests/init.pp
@@ -1,24 +1,19 @@
 class salt {
     package {["salt-minion"]:
-        ensure  => latest,
+        ensure  => absent,
         before => File["/etc/salt/minion"],
     }
     
     file {"/etc/salt/minion":
-        content => "master_finger: '39:ea:41:f1:3d:d5:4b:32:bb:1a:a1:ff:27:0e:08:7e:89:cd:8c:75:c5:f7:0e:f3:cd:19:53:98:e6:01:55:fe'
-",
-        notify => Service["salt-minion"],
+        ensure  => absent,
     }
     
     file {"/etc/salt/minion_id":
-        content => "$fqdn",
-        notify => Service["salt-minion"],
+        ensure => absent,
+    }
+    
+    file {"/etc/salt":
+        ensure => absent,
     }
 
-    service { 'salt-minion':
-        ensure  => running,
-        enable  => true,
-        subscribe => File["/etc/salt/minion"],
-        require => Package["salt-minion"],
-    }
 }
